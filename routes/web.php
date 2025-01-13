28b65894ff46f67aee863b136bd44258ce68f243
Pratik Bhujel <prateekbhujelpb@gmail.com>
2025-01-13 21:55:34 +0545

    Inital commit

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.layouts.master');
+    return view('admin.dashboard.index');
 });
 
 Route::get('/dashboard', function () {
