edac6d8a8d558bd18997c6b282610a6b961c1045
Pratik Bhujel <prateekbhujelpb@gmail.com>
2025-01-14 15:28:52 +0545

    [feat] Breeze auth login connected to the Dashboard and all

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -3,47 +3,51 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>General Dashboard &mdash; Stisla</title>
+  <title>@yield('title', 'General Dashboard') &mdash; {{ config('app.name') }}</title>
 
   <!-- General CSS Files -->
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin/assets/modules/bootstjrap/css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css') }}">
   <link rel="stylesheet" href="{{ asset('admin/assets/modules/summernote/summernote-bs4.css') }}">
   <!-- Template CSS -->
   <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
   <link rel="stylesheet" href="{{ asset('admin/assets/css/components.css') }}">
-<!-- Start GA -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-94034622-3');
-</script>
-<!-- /END GA --></head>
+  <!-- Start GA -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-94034622-3');
+  </script>
+  <!-- /END GA -->
+</head>
 
 <body>
   <div id="app">
     <div class="main-wrapper main-wrapper-1">
 
-      <!-- Sidebar Content -->
-        @include('admin.layouts.sidebar')
+      <!-- Header -->
+      @include('admin.layouts.header')
+
+      <!-- Sidebar -->
+      @include('admin.layouts.sidebar')
 
       <!-- Main Content -->
       <div class="main-content">
         <section class="section">
-            @yield('content')
+          @yield('content')
         </section>
-
       </div>
+
+      <!-- Footer -->
       <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+          Copyright &copy; {{ date('Y') }}
+          <div class="bullet"></div> Developed By 
+          <a target="_blank" href="https://https://pratikbhujel.com.np/">Pratik Bhujel</a>
         </div>
-        <div class="footer-right">
-          
-        </div>
+        <div class="footer-right"></div>
       </footer>
     </div>
   </div>
@@ -57,7 +61,7 @@
   <script src="{{ asset('admin/assets/modules/moment.min.js') }}"></script>
   <script src="{{ asset('admin/assets/js/stisla.js') }}"></script>
   
-  <!-- JS Libraies -->
+  <!-- JS Libraries -->
   <script src="{{ asset('admin/assets/modules/summernote/summernote-bs4.js') }}"></script>
   
   <!-- Template JS File -->
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,96 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Login &mdash; {{ config('app.name') }}</title>
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('admin/assets/modules/bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css') }}">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
+</head>
+<body>
+  <div id="app">
+    <section class="section">
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand">
+              <img src="{{ asset('admin/assets/img/stisla-fill.svg') }}" alt="logo" width="100" class="shadow-light rounded-circle">
+            </div>
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+            <div class="card card-primary">
+              <div class="card-header"><h4>Login</h4></div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+              <div class="card-body">
+                <form method="POST" action="{{ route('login') }}" novalidate>
+                  @csrf
+                  <div class="form-group">
+                    <label for="email">Email</label>
+                    <input 
+                      id="email" 
+                      type="email" 
+                      class="form-control @error('email') is-invalid @enderror" 
+                      name="email" 
+                      value="{{ old('email') }}" 
+                      required 
+                      autofocus>
+                    @error('email')
+                    <div class="invalid-feedback">
+                      {{ $message }}
+                    </div>
+                    @enderror
+                  </div>
+
+                  <div class="form-group">
+                    <div class="d-block">
+                      <label for="password" class="control-label">Password</label>
+                      <div class="float-right">
+                        <a href="{{ route('password.request') }}" class="text-small">
+                          Forgot Password?
+                        </a>
+                      </div>
+                    </div>
+                    <input 
+                      id="password" 
+                      type="password" 
+                      class="form-control @error('password') is-invalid @enderror" 
+                      name="password" 
+                      required>
+                    @error('password')
+                    <div class="invalid-feedback">
+                      {{ $message }}
+                    </div>
+                    @enderror
+                  </div>
+
+                  <div class="form-group">
+                    <div class="custom-control custom-checkbox">
+                      <input 
+                        type="checkbox" 
+                        name="remember" 
+                        class="custom-control-input" 
+                        id="remember-me">
+                      <label class="custom-control-label" for="remember-me">Remember Me</label>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block">
+                      Login
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="simple-footer">
+            Copyright All rights reserved. <i class="fa fa-copyright" aria-hidden="true"></i> {{ now()->year }} {{ config('app.name') }}
+            </div>
+          </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+      </div>
+    </section>
+  </div>
+</body>
+</html>
