dd62dfa831759b189377d5a6da05f5cd631078d9
Utkarsha-Aryal <utkarshaaryal7@gmail.com>
2025-03-14 17:58:44 +0545

    edit made for photoslider

---
+++ resources/views/admin/photoslider/edit.blade.php
@@ -0,0 +1,62 @@
+@extends('admin.layouts.master')
+
+@section('title', 'Photo Slider')
+
+@section('content')
+<div class="section-header">
+    <div class="section-header-back">
+        <a href="{{ route('admin.photoslider.index') }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
+    </div>
+    <h1>Manage Slider</h1>
+</div>
+
+<div class='card'>
+    <div class="card-header">
+        <h4>Photo Slider</h4>
+    </div>
+    <div class="card-body">
+        @php
+        $fields = [
+            [
+                'name' => 'main_title',
+                'label' => 'Main Title',
+                'type' => 'text',
+                'required' => 'true',
+                'placeholder' => 'enter main title',
+            ],
+            [
+                'name' => 'sub_title',
+                'label' => 'Sub Title',
+                'type' => 'text',
+                'placeholder' => 'enter sub title',
+                'required' => false
+            ],
+            [
+                'name' => 'category',
+                'label' => 'Category',
+                'type' => 'select', // Add this key
+    'options' => [
+        ''=> 'no Category',
+        'about'   => 'about', 
+        'board'   => 'board',
+        'contact' => 'contact',
+        'gallery' => 'gallery',
+        'history' => 'history'
+                ],
+                'required' => false, // Added missing comma here
+                'placeholder' => 'enter sub title',
+            ],
+            [
+                'name' => 'main_image',
+                'label' => 'Main Image',
+                'type' => 'file',
+                'required' => true
+            ],
+        ];
+        @endphp
+
+        <x-cms-form action="{{ route('admin.photoslider.update',$photoslider->id) }}" :fields="$fields" submitText="Update Slider" buttonPosition="right"
+        :model="$photoslider" />
+    </div>
+</div>
+@endsection
