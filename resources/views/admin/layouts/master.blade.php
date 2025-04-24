5499595bfc8648e3c7d1d14601b310139fccd763
Utkarsha-Aryal <utkarshaaryal7@gmail.com>
2025-04-24 13:20:57 +0545

    changes made

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'Nawalpursewasamaj') &mdash; {{ config('app.name') }}</title>
-    @stack('styles')
+    <title>{{@$siteSetting->organization_name}}</title>
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('admin/assets/modules/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css') }}">
--- resources/views/layouts/frontend/main.blade.php
+++ resources/views/layouts/frontend/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NRB Organization - Home</title>
+    <title>{{@$siteSetting->organization_name}}</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom Css -->
     <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
